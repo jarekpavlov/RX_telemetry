488ba9388863d0b6b81300d213d4d0013ceb65d8
jarek <iaroslav.pavlov@unibas.ch>
2025-11-24 16:21:24 +0100

    using 100th channel

--- RX_telemetry.ino
+++ RX_telemetry.ino
@@ -35,7 +35,7 @@ void setup()
                                       //Configure the NRF24 module
   radio.begin();
   radio.openReadingPipe(1,pipe);
-  radio.setChannel(99);
+  radio.setChannel(100);
   radio.setAutoAck(false);
   radio.setDataRate(RF24_250KBPS);    // The lowest data rate value for more stable communication
   radio.setPALevel(RF24_PA_MAX);      // Output power is set for maximum
